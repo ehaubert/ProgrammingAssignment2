a5547aeb16897e8ebbf8a4f2c4eddf9a9192b06e
Elizabeth Haubert <elizabeth.haubert@gmail.com>
2019-05-25 18:09:52 -0400

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,44 @@
-## Put comments here that give an overall description of what your
-## functions do
+## 
+## Construct a closure to wrap a matrix and cache its inversion.
+## 
 
-## Write a short comment describing this function
+# load MASS library for ginv for matrix inversion.
+library(MASS)
 
+
+## Create a closure to 
+## 1. set the value of a matrix
+## 2. get the value of the matrix
+## 3. set the value of the matrix inversion
+## 4. get the value of the matrix inversion
 makeCacheMatrix <- function(x = matrix()) {
-
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(ginv) inv <<- ginv
+  getinverse <- function() inv
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## This function computes the inverse of the special "matrix" returned by
+## makeCacheMatrix above. If the inverse has already been calculated (and 
+## the matrix has not changed), then cacheSolve should retrieve the inverse 
+## from the cache.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  inv <- x$getinv()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- ginv(data, ...)
+  x$setinv(inv)
+  inv
 }
